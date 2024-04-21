10581c89d56f68ff2068031127dbef5c4e11f7cc
Krr0ptioN <mardin-pop@protonmail.com>
2024-04-21 11:58:09 +0300

    chore(api): setup vitest

--- apps/api/vite.config.ts
+++ apps/api/vite.config.ts
@@ -1,13 +1,19 @@
-/// <reference types='vitest' />
 import { defineConfig } from 'vite';
-
+import swc from 'unplugin-swc';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+  build: {
+    outDir: '../../dist/apps/api',
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    emptyOutDir: true,
+  },
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/api',
-
-  plugins: [nxViteTsPaths()],
+  plugins: [nxViteTsPaths(), swc.vite(), swc.rollup()],
 
   // Uncomment this if you are using workers.
   // worker: {
@@ -16,16 +22,13 @@ export default defineConfig({
 
   test: {
     globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest',
-    },
+    cache: { dir: '../../node_modules/.vitest' },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-
     reporters: ['default'],
     coverage: {
       reportsDirectory: '../../coverage/apps/api',
-      provider: 'v8',
+      provider: 'istanbul',
     },
   },
 });
