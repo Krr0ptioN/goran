5e1b9131795cb88a2f7792737c95341abe692dfa
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-03 16:54:59 +0300

    feat(data-acess): neon data acess

---
+++ libs/data-access/db-data-access/src/db-data-access.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { dbDataAccessProvider } from './db-data-access.provider';
+
+@Module({
+    providers: [dbDataAccessProvider],
+    exports: [dbDataAccessProvider],
+})
+export class DbDataAccessModule { }
---
+++ libs/data-access/db-data-access/src/db-data-access.provider.ts
@@ -0,0 +1,21 @@
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+import { ConfigService } from '@nestjs/config';
+import { CONFIG_APP } from '@goran/config';
+import * as schema from './schema';
+
+export const DbDataAccessProvider = 'DbDataAccessProvider';
+
+export const dbDataAccessProvider = {
+  provide: DbDataAccessProvider,
+  useFactory: async () => {
+    const url = process.env[CONFIG_APP.DRIZZLE_DATABASE_URL];
+    if (url) {
+      const sql = neon(url);
+      return drizzle(sql, { schema });
+    } else {
+      throw new Error('DRIZZLE_DATABASE_URL env is not set');
+    }
+  },
+  exports: [DbDataAccessProvider],
+};
