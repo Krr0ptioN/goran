11bd6fe8a98fc5563c5a92caf93265ae826c33c5
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-03 16:52:10 +0300

    feat(api): parse cookie

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,6 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
@@ -8,6 +8,8 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  app.use(cookieParser());
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
