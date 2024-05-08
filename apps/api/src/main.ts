70a5bb166e8d493498b8f4e95ed7702642396c77
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-08 17:45:33 +0300

    fix: add dto validation pipe

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,11 +1,13 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3000;
 
   await app.listen(port);
