c51199f354cf2caca30c806024eb17afd79e9fbe
Krr0ptioN <mardin-pop@protonmail.com>
2024-05-08 17:46:45 +0300

    test(security): e2e user authentication and validation

--- apps/api-e2e/src/api/auth.spec.ts
+++ apps/api-e2e/src/api/auth.spec.ts
@@ -1,52 +1,92 @@
-import { INestApplication } from '@nestjs/common';
+import { USERS_REPOSITORY } from '@goran/users';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 
 /* eslint-disable @nx/enforce-module-boundaries */
 import { AppModule } from 'apps/api/src/app/app.module';
+import { MockUsersRepository } from 'libs/domain/users/src/repositories/user-mock.repository';
 /* eslint-enable @nx/enforce-module-boundaries */
 
 import request from 'supertest';
 
 const user = {
-  email: 'joe.rogan@goran.com',
-  username: 'joerogan',
-  fullname: 'Joe Rogan',
-  password: 'joeStr01ngP4',
+    email: 'rogan@goran.com',
+    username: 'rogan',
+    fullname: 'Joe Rogan',
+    password: 'JoeR0gan',
 };
 
 describe('Authentication /api/auth', () => {
-  let app: INestApplication;
+    let app: INestApplication;
 
-  beforeAll(async () => {
-    const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+    beforeAll(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [AppModule],
+        })
+            .overrideProvider(USERS_REPOSITORY)
+            .useClass(MockUsersRepository)
+            .compile();
+        app = moduleRef.createNestApplication();
+        app.useGlobalPipes(new ValidationPipe());
+        await app.init();
+    });
 
-    app = moduleRef.createNestApplication();
-    await app.init();
-  });
+    test(`User registeration`, async () => {
+        const res = await request(app.getHttpServer())
+            .post('/auth/signup')
+            .send(user);
+        expect(res.body.data.tokens).toBeDefined();
+        expect(res.body.data.user.email).toEqual(user.email);
+        expect(res.body.data.user.username).toEqual(user.username);
+        expect(res.body.data.user.password).not.toBeDefined();
+        expect(res.status).toEqual(201);
+    });
 
-  /* TEST: Signup new user
-   * */
-  test(`User registeration`, async () => {
-    const res = await request(app.getHttpServer())
-      .post('/auth/signup')
-      .send(user);
-    console.log(res);
-    expect(res).toBeDefined();
-  });
+    it(`should not signup user with weak password`, async () => {
+        const res = await request(app.getHttpServer())
+            .post('/auth/signup')
+            .send({ ...user, password: 'weakpass' });
+        expect(res.body.error).toMatch('Bad Request');
+        expect(res.body.message).toEqual([
+            'Password must contain at least one lowercase letter, one uppercase letter, and one digit',
+        ]);
+        expect(res.body.statusCode).toEqual(400);
+    });
 
-  /* TEST: Signin new user
-   * */
+    it(`login using email`, async () => {
+        const res = await request(app.getHttpServer())
+            .post('/auth/signin')
+            .send({ email: user.email, password: user.password });
+        expect(res.body.data.tokens).toBeDefined();
+        expect(res.body.data.user.email).toEqual(user.email);
+        expect(res.body.data.user.username).toEqual(user.username);
+        expect(res.body.data.user.password).not.toBeDefined();
+        expect(res.status).toEqual(201);
+    });
 
-  it(`User login`, () => {
-    return request(app.getHttpServer())
-      .post('/auth/signin')
-      .send({ email: user.email, password: user.password })
-      .expect(200);
-  });
+    it(`login using username`, async () => {
+        const res = await request(app.getHttpServer())
+            .post('/auth/signin')
+            .send({ username: user.username, password: user.password });
+        expect(res.body.data.tokens).toBeDefined();
+        expect(res.body.data.user.email).toEqual(user.email);
+        expect(res.body.data.user.username).toEqual(user.username);
+        expect(res.body.data.user.password).not.toBeDefined();
+        expect(res.status).toEqual(201);
+    });
 
-  afterAll(async () => {
-    await app.close();
-  });
+    it(`should not login with invalid password`, async () => {
+        const res = await request(app.getHttpServer())
+            .post('/auth/signin')
+            .send({ email: user.email, password: user.password + 'invalid' });
+        expect(res.body.error).toBeDefined();
+        expect(res.body.message).toEqual(
+            'User not found or the provided credential is invalid.'
+        );
+        expect(res.status).toEqual(400);
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
 });
