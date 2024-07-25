13ed667301926097c9f89de14b0cbc6125940826
Krr0ptioN <mardin-pop@protonmail.com>
2024-07-25 12:58:59 +0300

    docs(api): global pipe and interceptors registers util function

--- apps/api/src/globals.ts
+++ apps/api/src/globals.ts
@@ -1,15 +1,21 @@
 import {
-  ClassSerializerInterceptor,
-  INestApplication,
-  ValidationPipe,
+    ClassSerializerInterceptor,
+    INestApplication,
+    ValidationPipe,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 
+/**
+ * Registers global pipes and interceptors, plus server conifguration
+ *
+ * @param app - Nestjs application object
+ * @returns Modified and configured nestjs application object and certain parameters
+ */
 export async function registerGlobals(app: INestApplication) {
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  app.useGlobalPipes(new ValidationPipe());
-  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+    const globalPrefix = 'api';
+    app.setGlobalPrefix(globalPrefix);
+    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
-  return { app, globalPrefix };
+    return { app, globalPrefix };
 }
