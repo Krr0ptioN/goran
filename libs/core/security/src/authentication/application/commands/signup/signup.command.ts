5eeaf4a9870ffd3e3275e1258acc013b80a369c7
Krr0ptioN <mardin-pop@protonmail.com>
2024-07-29 03:55:48 +0300

    feat(security-authentication): signup command

---
+++ libs/core/security/src/authentication/application/commands/signup/signup.command.ts
@@ -0,0 +1,24 @@
+import { Command, CommandProps } from '@goran/common';
+
+/**
+ * Signup - User Registration
+ *
+ * @param fullname
+ * @param username
+ * @param password
+ * @param email
+ */
+export class SignupCommand extends Command {
+    readonly email: string;
+    readonly fullname: string;
+    readonly username: string;
+    readonly password: string;
+
+    constructor(props: CommandProps<SignupCommand>) {
+        super(props);
+        this.fullname = props.fullname;
+        this.username = props.username;
+        this.email = props.email;
+        this.password = props.password;
+    }
+}
