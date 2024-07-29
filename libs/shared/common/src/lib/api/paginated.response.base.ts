acffd8269818fb8ed6aa17537dd5277f8c33c445
Krr0ptioN <mardin-pop@protonmail.com>
2024-07-29 03:52:57 +0300

    fix(pagination): overridable paginated response object

--- libs/shared/common/src/lib/api/paginated.response.base.ts
+++ libs/shared/common/src/lib/api/paginated.response.base.ts
@@ -2,21 +2,21 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Paginated } from '../ddd';
 
 export abstract class PaginatedResponseDto<T> extends Paginated<T> {
-  @ApiProperty({
-    example: 5312,
-    description: 'Total number of items',
-  })
-  readonly count: number;
+    @ApiProperty({
+        example: 5312,
+        description: 'Total number of items',
+    })
+    override readonly count: number;
 
-  @ApiProperty({
-    example: 10,
-    description: 'Number of items per page',
-  })
-  readonly limit: number;
+    @ApiProperty({
+        example: 10,
+        description: 'Number of items per page',
+    })
+    override readonly limit: number;
 
-  @ApiProperty({ example: 0, description: 'Page number' })
-  readonly page: number;
+    @ApiProperty({ example: 0, description: 'Page number' })
+    override readonly page: number;
 
-  @ApiProperty({ isArray: true })
-  abstract readonly data: readonly T[];
+    @ApiProperty({ isArray: true })
+    abstract override readonly data: readonly T[];
 }
