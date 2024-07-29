0ffee3303bdb9dc95779be19cfc5792844598d80
Krr0ptioN <mardin-pop@protonmail.com>
2024-07-29 21:51:48 +0300

    refactor: simplify token validation in LocalAuthGuard

--- libs/core/security/src/authentication/application/guards/local.guard.ts
+++ libs/core/security/src/authentication/application/guards/local.guard.ts
@@ -1,44 +1,43 @@
 import {
-    CanActivate,
-    ExecutionContext,
-    Injectable,
-    Logger,
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { AuthenticationTokenService } from '../services/token.service';
+import { AuthenticationTokenService } from '../services';
 import { Request } from 'express';
 import { UnauthorizedException } from '@nestjs/common';
 import { InvalidTokenError } from '../../domain/errors';
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') implements CanActivate {
-    logger = new Logger(LocalAuthGuard.name);
-    constructor(private readonly tokenService: AuthenticationTokenService) {
-        super();
+  logger = new Logger(LocalAuthGuard.name);
+  constructor(private readonly tokenService: AuthenticationTokenService) {
+    super();
+  }
+
+  override async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest<Request>();
+    const token = req.headers?.authorization?.split(' ')[1];
+    if (!token) {
+      throw new UnauthorizedException('Invalid token');
     }
 
-    override async canActivate(context: ExecutionContext): Promise<boolean> {
-        const req = context.switchToHttp().getRequest<Request>();
-        const token = req.headers?.authorization?.split(' ')[1];
-        if (!token) {
-            throw new UnauthorizedException('Invalid token');
-        }
-
-        try {
-            const user = await this.tokenService.getUserFromToken(token);
-            if (user) {
-                const isTokenRevoked = await this.tokenService.isTokenRevoked(token);
-                if (isTokenRevoked == true) {
-                    throw new UnauthorizedException('Token has been revoked');
-                }
-                req.user = user;
-                return true;
-            } else {
-                throw new InvalidTokenError('Invalid token provided');
-            }
-        } catch (error) {
-            this.logger.error(error);
-            throw new UnauthorizedException('Invalid user');
-        }
+    try {
+      const user = await this.tokenService.getUserFromToken(token);
+      if (user.isErr()) {
+        throw new InvalidTokenError('Invalid token provided');
+      }
+      const isTokenRevoked = await this.tokenService.isTokenRevoked(token);
+      if (isTokenRevoked === true) {
+        throw new UnauthorizedException('Token has been revoked');
+      }
+      req.user = user;
+      return true;
+    } catch (error) {
+      this.logger.error(error);
+      throw new UnauthorizedException('Invalid user');
     }
+  }
 }
