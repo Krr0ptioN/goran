1d5484eaeb6edd6232d2afb5db9c945777f775c0
Krr0ptioN <mardin-pop@protonmail.com>
2024-07-29 03:35:28 +0300

    add(users-management): username already ergistered and username or email providing errors

--- libs/domain/users/src/domain/errors/index.ts
+++ libs/domain/users/src/domain/errors/index.ts
@@ -1,3 +1,5 @@
 export * from './user-not-found.error';
 export * from './user-already-exists.error';
 export * from './email-already-registered.error';
+export * from './username-already-registered.error';
+export * from './provide-username-or-email.error';
