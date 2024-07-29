465cee298c48f487a97d6aa99c2dffd54d972a0b
Krr0ptioN <mardin-pop@protonmail.com>
2024-07-29 21:50:34 +0300

    fix: make fullname optional in SignupCommand

--- libs/core/security/src/authentication/application/commands/signup/signup.command.ts
+++ libs/core/security/src/authentication/application/commands/signup/signup.command.ts
@@ -9,16 +9,16 @@ import { Command, CommandProps } from '@goran/common';
  * @param email
  */
 export class SignupCommand extends Command {
-    readonly email: string;
-    readonly fullname: string;
-    readonly username: string;
-    readonly password: string;
+  readonly email: string;
+  readonly fullname?: string | null;
+  readonly username: string;
+  readonly password: string;
 
-    constructor(props: CommandProps<SignupCommand>) {
-        super(props);
-        this.fullname = props.fullname;
-        this.username = props.username;
-        this.email = props.email;
-        this.password = props.password;
-    }
+  constructor(props: CommandProps<SignupCommand>) {
+    super(props);
+    this.fullname = props.fullname;
+    this.username = props.username;
+    this.email = props.email;
+    this.password = props.password;
+  }
 }
