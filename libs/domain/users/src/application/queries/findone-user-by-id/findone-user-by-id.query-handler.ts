1c4d72a01db336a6cd8695f8cd68ac970ea6d07a
Krr0ptioN <mardin-pop@protonmail.com>
2024-07-29 21:52:57 +0300

    refactor: simplify error handling in FindOneUserByIdQueryHandler

--- libs/domain/users/src/application/queries/findone-user-by-id/findone-user-by-id.query-handler.ts
+++ libs/domain/users/src/application/queries/findone-user-by-id/findone-user-by-id.query-handler.ts
@@ -1,23 +1,19 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { FindOneUserByIdQuery } from './findone-user-by-id.query';
 import { Err, Ok, Result } from 'oxide.ts';
-import { UsersRepository, UserModel, UserNotFoundError } from '@goran/users';
+import { UsersRepository, UserModel, UserNotFoundError } from '../../..';
 import { ExceptionBase } from '@goran/common';
 
 @QueryHandler(FindOneUserByIdQuery)
 export class FindOneUserByIdQueryHandler implements IQueryHandler {
-  constructor(private readonly userRepo: UsersRepository) {}
+    constructor(private readonly userRepo: UsersRepository) { }
 
-  async execute(
-    query: FindOneUserByIdQuery
-  ): Promise<Result<UserModel, ExceptionBase>> {
-    const user = await this.userRepo.findOneById(query.id);
-    return user.isSome()
-      ? Ok(user.unwrap())
-      : Err(
-          new UserNotFoundError(
-            new Error('No such user found with corresponding id')
-          )
-        );
-  }
+    async execute(
+        query: FindOneUserByIdQuery
+    ): Promise<Result<UserModel, ExceptionBase>> {
+        const user = await this.userRepo.findOneById(query.id);
+        return user.isSome()
+            ? Ok(user.unwrap())
+            : Err(new UserNotFoundError());
+    }
 }
