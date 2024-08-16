4fdfc7b730eb307fdda3231de2ae96d13f2e0269
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-16 22:40:29 +0300

    chore: adding missing providers and imports

--- libs/domain/users/src/users.module.ts
+++ libs/domain/users/src/users.module.ts
@@ -3,30 +3,45 @@ import { UsersService } from './application/services';
 import { configSchema } from '@goran/config';
 import { DrizzleDataAccessModule } from '@goran/drizzle-data-access';
 import { ConfigModule } from '@nestjs/config';
-import { CreateUserCommandHandler, DeleteUserCommandHandler } from '@goran/users';
+import { CreateUserCommandHandler } from './application/commands/create-user';
+import { DeleteUserCommandHandler } from './application/commands/delete-user/delete-user.command-handler';
+import { UsersRepositoryProvider } from './application/ports/users.repository';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { PostgreSqlDrizzleUsersRepository } from './infrastructure/persistence/drizzle/postgres-drizzle-users.repository';
+import { UserMapper } from './application/mappers/user.mapper';
+import { CqrsModule } from '@nestjs/cqrs';
 
-const commandHandlers = [CreateUserCommandHandler, DeleteUserCommandHandler];
+const repo = {
+    provide: UsersRepositoryProvider,
+    useClass: PostgreSqlDrizzleUsersRepository,
+};
 
 @Module({
-  imports: [
-    EventEmitterModule.forRoot({
-      wildcard: false,
-      delimiter: '.',
-      newListener: false,
-      removeListener: false,
-      maxListeners: 10,
-      verboseMemoryLeak: false,
-      ignoreErrors: false,
-    }),
-    DrizzleDataAccessModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: ['.env', '.env.local'],
-      validationSchema: configSchema,
-    }),
-  ],
-  providers: [...commandHandlers, UsersService],
-  exports: [UsersService],
+    imports: [
+        CqrsModule,
+        EventEmitterModule.forRoot({
+            wildcard: false,
+            delimiter: '.',
+            newListener: false,
+            removeListener: false,
+            maxListeners: 10,
+            verboseMemoryLeak: false,
+            ignoreErrors: false,
+        }),
+        DrizzleDataAccessModule,
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: ['.env', '.env.local'],
+            validationSchema: configSchema,
+        }),
+    ],
+    providers: [
+        CreateUserCommandHandler,
+        DeleteUserCommandHandler,
+        UserMapper,
+        repo,
+        UsersService,
+    ],
+    exports: [UsersService],
 })
 export class UsersModule { }
