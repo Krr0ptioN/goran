8a591437b7d69217cb2dd1a53593a58104802558
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-18 00:57:09 +0300

    chore: missing handlers and cqrs command bus

--- libs/core/security/src/lib/authentication/authentication.module.ts
+++ libs/core/security/src/lib/authentication/authentication.module.ts
@@ -2,43 +2,49 @@ import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 import { UsersModule } from "@goran/users";
 import {
-	AuthenticationController,
-	AuthenticationTokenController,
-	AuthenticationPasswordController,
+  AuthenticationController,
+  AuthenticationTokenController,
+  AuthenticationPasswordController,
 } from "./presenters/http";
 import {
-	AuthenticationTokenService,
-	AuthenticationPasswordService,
+  AuthenticationTokenService,
+  AuthenticationPasswordService,
 } from "./application/services";
 import {
-	RequestPasswordResetCommandHandler,
-	VerifyPasswordResetAttemptCommandHandler,
-	ResetPasswordCommandHandler,
+  RequestPasswordResetCommandHandler,
+  VerifyPasswordResetAttemptCommandHandler,
+  ResetPasswordCommandHandler,
+  SignupCommandHandler,
+  SigninCommandHandler,
 } from "./application/commands";
 import { CacheModule } from "@nestjs/cache-manager";
 import { MailModule } from "@goran/mail";
+import { CqrsModule } from "@nestjs/cqrs";
 
 const services = [AuthenticationTokenService, AuthenticationPasswordService];
 
 const commandHanlders = [
-	RequestPasswordResetCommandHandler,
-	ResetPasswordCommandHandler,
-	VerifyPasswordResetAttemptCommandHandler,
+  SignupCommandHandler,
+  SigninCommandHandler,
+  RequestPasswordResetCommandHandler,
+  ResetPasswordCommandHandler,
+  VerifyPasswordResetAttemptCommandHandler,
 ];
 
 @Module({
-	imports: [
-		UsersModule,
-		JwtModule.register({ global: true }),
-		CacheModule.register({ isGlobal: true }),
-		MailModule,
-	],
-	exports: [...services],
-	controllers: [
-		AuthenticationController,
-		AuthenticationPasswordController,
-		AuthenticationTokenController,
-	],
-	providers: [...commandHanlders, ...services],
+  imports: [
+    UsersModule,
+    CqrsModule,
+    JwtModule.register({ global: true }),
+    CacheModule.register({ isGlobal: true }),
+    MailModule,
+  ],
+  exports: [...services],
+  controllers: [
+    AuthenticationController,
+    AuthenticationPasswordController,
+    AuthenticationTokenController,
+  ],
+  providers: [...commandHanlders, ...services],
 })
-export class AuthenticationModule {}
+export class AuthenticationModule { }
