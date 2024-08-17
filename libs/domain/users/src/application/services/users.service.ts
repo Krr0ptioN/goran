1fd8d777268d04bf3c9d20d2ebd71e9219bb7eed
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-18 00:44:51 +0300

    fix: async services

--- libs/domain/users/src/application/services/users.service.ts
+++ libs/domain/users/src/application/services/users.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import {
-    FindAllUsersPaginatedQuery,
-    FindOneUserByEmailQuery,
-    FindOneUserByIdQuery,
-    FindOneUserByUsernameQuery,
+  FindAllUsersPaginatedQuery,
+  FindOneUserByEmailQuery,
+  FindOneUserByIdQuery,
+  FindOneUserByUsernameQuery,
 } from '../queries';
 import { UserDto, UserModel } from '../models';
 import { UserMapper } from '../mappers';
 import {
-    ChangeUserEmailCommand,
-    ChangeUserPasswordCommand,
-    ChangeUserUsernameCommand,
-    CreateUserCommand,
-    DeleteUserCommand,
+  ChangeUserEmailCommand,
+  ChangeUserPasswordCommand,
+  ChangeUserUsernameCommand,
+  CreateUserCommand,
+  DeleteUserCommand,
 } from '../commands';
 import {
-    UserNotFoundError,
-    ProvideUsernameOrEmailError,
-    UserEntity,
+  UserNotFoundError,
+  ProvideUsernameOrEmailError,
+  UserEntity,
 } from '../../domain';
 import { ExceptionBase } from '@goran/common';
 import { Err, Ok, Option, Result } from 'oxide.ts';
@@ -25,75 +25,77 @@ import { CommandBus, QueryBus } from '@nestjs/cqrs';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        private readonly commandBus: CommandBus,
-        private readonly queryBus: QueryBus,
-        private readonly mapper: UserMapper
-    ) { }
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+    private readonly mapper: UserMapper
+  ) { }
 
-    changeEmail(userDto: UserModel, newEmail: string) {
-        const user = this.mapper.toDomain(userDto);
-        return this.commandBus.execute(
-            new ChangeUserEmailCommand({ user, newEmail })
-        );
-    }
+  async changeEmail(userDto: UserModel, newEmail: string) {
+    const user = this.mapper.toDomain(userDto);
+    return await this.commandBus.execute(
+      new ChangeUserEmailCommand({ user, newEmail })
+    );
+  }
 
-    changePassword(userDto: UserModel, newHashedPassword: string) {
-        const user = this.mapper.toDomain(userDto);
-        return this.commandBus.execute(
-            new ChangeUserPasswordCommand({ user, newHashedPassword })
-        );
-    }
-    changeUsername(userDto: UserModel, newUsername: string) {
-        const user = this.mapper.toDomain(userDto);
-        return this.commandBus.execute(
-            new ChangeUserUsernameCommand({ user, newUsername })
-        );
-    }
+  async changePassword(userDto: UserModel, newHashedPassword: string) {
+    const user = this.mapper.toDomain(userDto);
+    return await this.commandBus.execute(
+      new ChangeUserPasswordCommand({ user, newHashedPassword })
+    );
+  }
 
-    create(userDto: UserDto) {
-        return this.commandBus.execute(new CreateUserCommand(userDto));
-    }
+  async changeUsername(userDto: UserModel, newUsername: string) {
+    const user = this.mapper.toDomain(userDto);
+    return await this.commandBus.execute(
+      new ChangeUserUsernameCommand({ user, newUsername })
+    );
+  }
 
-    findAllPaginated(query: FindAllUsersPaginatedQuery) {
-        return this.queryBus.execute(query);
-    }
+  async create(userDto: UserDto) {
+    return await this.commandBus.execute(new CreateUserCommand(userDto));
+  }
 
-    findOneByEmail(email: string) {
-        return this.queryBus.execute(new FindOneUserByEmailQuery(email));
-    }
-    findOneByUsername(username: string) {
-        return this.queryBus.execute(new FindOneUserByUsernameQuery(username));
-    }
+  async findAllPaginated(query: FindAllUsersPaginatedQuery) {
+    return await this.queryBus.execute(query);
+  }
 
-    async findUserByIdenfitier({
-        username,
-        email,
-    }: {
-        username?: string;
-        email?: string;
-    }): Promise<Result<UserEntity, ExceptionBase>> {
-        let foundedUser: Option<UserEntity>;
-        if (email) {
-            foundedUser = await this.findOneByEmail(email);
-        } else if (username) {
-            foundedUser = await this.findOneByUsername(username);
-        } else {
-            return Err(new ProvideUsernameOrEmailError());
-        }
+  async findOneByEmail(email: string) {
+    return await this.queryBus.execute(new FindOneUserByEmailQuery(email));
+  }
 
-        if (foundedUser.isSome()) {
-            return Ok(foundedUser.unwrap());
-        } else {
-            return Err(new UserNotFoundError());
-        }
+  async findOneByUsername(username: string) {
+    return await this.queryBus.execute(new FindOneUserByUsernameQuery(username));
+  }
+
+  async findUserByIdenfitier({
+    username,
+    email,
+  }: {
+    username?: string;
+    email?: string;
+  }): Promise<Result<UserEntity, ExceptionBase>> {
+    let foundedUser: Option<UserEntity>;
+    if (email) {
+      foundedUser = await this.findOneByEmail(email);
+    } else if (username) {
+      foundedUser = await this.findOneByUsername(username);
+    } else {
+      return Err(new ProvideUsernameOrEmailError());
     }
 
-    findOneById(id: string) {
-        return this.queryBus.execute(new FindOneUserByIdQuery(id));
+    if (foundedUser.isSome()) {
+      return Ok(foundedUser.unwrap());
+    } else {
+      return Err(new UserNotFoundError());
     }
+  }
 
-    delete(command: DeleteUserCommand) {
-        return this.commandBus.execute(command);
-    }
+  async findOneById(id: string) {
+    return await this.queryBus.execute(new FindOneUserByIdQuery(id));
+  }
+
+  async delete(command: DeleteUserCommand) {
+    return await this.commandBus.execute(command);
+  }
 }
