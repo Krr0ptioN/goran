3a9dded1eafc61f0f6d55aeec820fd71d133422f
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-18 00:45:39 +0300

    chore: implementing validation (no-rules)

--- libs/domain/users/src/domain/entities/user/user.entity.ts
+++ libs/domain/users/src/domain/entities/user/user.entity.ts
@@ -4,30 +4,32 @@ import { CreateUserProps, UserProps } from './user.types';
 import { ulid } from 'ulid';
 
 export class UserEntity extends AggregateRoot<UserProps> {
-  protected readonly _id: AggregateID;
+    protected readonly _id: AggregateID;
 
-  static create(create: CreateUserProps): UserEntity {
-    const id = ulid();
-    const props: UserProps = { ...create };
-    const user = new UserEntity({ id, props });
-    user.addEvent(
-      new UserCreatedDomainEvent({
-        aggregateId: id,
-        email: props.email,
-        username: props.username,
-        fullname: props.fullname,
-      })
-    );
-    return user;
-  }
+    static create(create: CreateUserProps): UserEntity {
+        const id = ulid();
+        const props: UserProps = { ...create };
+        const user = new UserEntity({ id, props });
+        user.addEvent(
+            new UserCreatedDomainEvent({
+                aggregateId: id,
+                email: props.email,
+                username: props.username,
+                fullname: props.fullname,
+            })
+        );
+        return user;
+    }
 
-  delete(): void {
-    this.addEvent(
-      new UserDeletedDomainEvent({
-        aggregateId: this.id,
-      })
-    );
-  }
+    delete(): void {
+        this.addEvent(
+            new UserDeletedDomainEvent({
+                aggregateId: this.id,
+            })
+        );
+    }
 
-  validate(): void { }
+    validate(): void {
+        return;
+    }
 }
