b7b96227639f6b7001f90d2e1de4a052e3839992
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-18 00:44:38 +0300

    refact: simplifying props passing

--- libs/domain/users/src/application/commands/create-user/create-user.command-handler.ts
+++ libs/domain/users/src/application/commands/create-user/create-user.command-handler.ts
@@ -2,35 +2,35 @@ import { Inject, Logger } from '@nestjs/common';
 import { CreateUserCommand } from './create-user.command';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import {
-    UsersRepository,
-    UsersRepositoryProvider,
+  UsersRepository,
+  UsersRepositoryProvider,
 } from '../../ports/users.repository';
 import {
-    UserEntity,
+  UserEntity,
 } from '../../../domain/entities';
 
 @CommandHandler(CreateUserCommand)
 export class CreateUserCommandHandler
-    implements ICommandHandler<CreateUserCommand> {
-    private readonly logger = new Logger(CreateUserCommandHandler.name);
+  implements ICommandHandler<CreateUserCommand> {
+  private readonly logger = new Logger(CreateUserCommandHandler.name);
 
-    constructor(
-        @Inject(UsersRepositoryProvider) private readonly userRepo: UsersRepository,
-    ) { }
+  constructor(
+    @Inject(UsersRepositoryProvider) private readonly userRepo: UsersRepository,
+  ) { }
 
-    async execute(command: CreateUserCommand) {
-        this.logger.debug(
-            `Processing user specifics with ${command.email} email`
-        );
-        this.logger.debug(`User specifics: ${JSON.stringify(command)}`);
+  async execute(command: CreateUserCommand) {
+    this.logger.debug(
+      `Processing user specifics with ${command.email} email`
+    );
+    this.logger.debug(`User specifics: ${JSON.stringify(command)}`);
 
-        const user = UserEntity.create({ ...command });
+    const user = UserEntity.create(command);
 
-        const repoResult = await this.userRepo.insertOne(user);
+    const repoResult = await this.userRepo.insertOne(user);
 
-        if (repoResult.isOk())
-            this.logger.verbose(`User created with ${user.getProps().email} email`);
+    if (repoResult.isOk())
+      this.logger.verbose(`User created with ${user.getProps().email} email`);
 
-        return repoResult;
-    }
+    return repoResult;
+  }
 }
