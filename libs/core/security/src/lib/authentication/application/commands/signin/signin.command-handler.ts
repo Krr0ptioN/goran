4b20df0b371b44f1a28b130f370413b43ac9a3c3
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-27 01:19:21 +0300

    refact(auth/sign-in-signup-cmds): using password service and token factory

--- libs/core/security/src/lib/authentication/application/commands/signin/signin.command-handler.ts
+++ libs/core/security/src/lib/authentication/application/commands/signin/signin.command-handler.ts
@@ -1,52 +1,54 @@
 import { Logger } from '@nestjs/common';
 import { SigninCommand } from './signin.command';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
-import {
-  AuthenticationPasswordService,
-  AuthenticationTokenService,
-} from '../../services';
+import { PasswordService } from '../../services';
 import { UsersService } from '@goran/users';
 import { Result, Err, Ok } from 'oxide.ts';
-import { InvalidAuthenticationCredentials, } from '../../../domain';
+import { InvalidAuthenticationCredentials } from '../../../domain';
 import { AuthenticationCredentialDto } from '../../dtos';
 import { ExceptionBase } from '@goran/common';
+import { AuthenticationTokenFactory } from '../../factories';
 
 @CommandHandler(SigninCommand)
 export class SigninCommandHandler implements ICommandHandler<SigninCommand> {
-  private readonly logger = new Logger(SigninCommand.name);
+    private readonly logger = new Logger(SigninCommand.name);
 
-  constructor(
-    private tokenService: AuthenticationTokenService,
-    private passwordService: AuthenticationPasswordService,
-    private usersService: UsersService
-  ) { }
+    constructor(
+        private tokenFactory: AuthenticationTokenFactory,
+        private passwordService: PasswordService,
+        private usersService: UsersService
+    ) {}
 
-  async execute(command: SigninCommand): Promise<Result<AuthenticationCredentialDto, ExceptionBase>> {
-    const { username, email, password } = command;
-    const userResult = await this.usersService.findUserByIdenfitier({
-      username,
-      email,
-    });
+    async execute(
+        command: SigninCommand
+    ): Promise<Result<AuthenticationCredentialDto, ExceptionBase>> {
+        const { username, email, password } = command;
+        const userResult = await this.usersService.findUserByIdenfitier({
+            username,
+            email,
+        });
 
-    if (userResult.isErr()) {
-      return Err(new InvalidAuthenticationCredentials());
+        if (userResult.isErr()) {
+            return Err(new InvalidAuthenticationCredentials());
+        }
+        const user = userResult.unwrap();
+        const passwordIsValid = await this.passwordService.validatePassword(
+            password,
+            user.getProps().password
+        );
+
+        if (!passwordIsValid) {
+            return Err(new InvalidAuthenticationCredentials());
+        }
+
+        const tokens = this.tokenFactory.generateTokens({
+            userId: userResult.unwrap().getProps().id,
+        });
+
+        this.logger.verbose(
+            `User with ${user.getProps().email} email is authenticated`
+        );
+
+        return Ok(new AuthenticationCredentialDto({ user, tokens }));
     }
-    const user = userResult.unwrap();
-    const passwordIsValid = await this.passwordService.validatePassword(
-      password,
-      user.getProps().password
-    );
-
-    if (!passwordIsValid) {
-      return Err(new InvalidAuthenticationCredentials());
-    }
-
-    const tokens = this.tokenService.generateTokens({
-      userId: userResult.unwrap().getProps().id,
-    });
-
-    this.logger.verbose(`User with ${user.getProps().email} email is authenticated`);
-
-    return Ok(new AuthenticationCredentialDto({ user, tokens }));
-  }
 }
