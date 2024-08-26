533f7b820152842bbb8fc3aa2f99a3601aae2f96
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-27 01:14:32 +0300

    chore: linting single qoutes

--- libs/core/security/src/lib/authentication/presenters/http/index.ts
+++ libs/core/security/src/lib/authentication/presenters/http/index.ts
@@ -1,3 +1,3 @@
-export * from "./authentication";
-export * from "./authentication-token";
+export * from './authentication';
+export * from './authentication-token';
 export * from './password-reset';
