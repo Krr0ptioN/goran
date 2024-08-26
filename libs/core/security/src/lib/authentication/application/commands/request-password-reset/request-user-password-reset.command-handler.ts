6bbbc62fb8fb3a4d8aefd41b2da02397cf41c93a
Krr0ptioN <mardin-pop@protonmail.com>
2024-08-27 01:30:10 +0300

    feat(auth/request-password-reset-cmd): saving the aggregate state and returning the token

--- libs/core/security/src/lib/authentication/application/commands/request-password-reset/request-user-password-reset.command-handler.ts
+++ libs/core/security/src/lib/authentication/application/commands/request-password-reset/request-user-password-reset.command-handler.ts
@@ -1,9 +1,14 @@
-import { ConflictException, Logger } from '@nestjs/common';
+import {
+    ConflictException,
+    InternalServerErrorException,
+    Logger,
+} from '@nestjs/common';
 import { RequestUserPassswordResetCommand } from './request-user-password-reset.command';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { PasswordResetTokenFactory } from '../../factories';
 import { UsersService } from '@goran/users';
 import { PasswordResetRequestAggregate } from '../../../domain';
+import { PasswordResetRequestRepository } from '../../ports';
 
 @CommandHandler(RequestUserPassswordResetCommand)
 export class RequestPasswordResetCommandHandler
@@ -13,21 +18,37 @@ export class RequestPasswordResetCommandHandler
 
     constructor(
         private readonly tokenFactory: PasswordResetTokenFactory,
-        private readonly userService: UsersService
+        private readonly userService: UsersService,
+        private readonly passwordResetReqRepo: PasswordResetRequestRepository
     ) {}
 
     async execute(command: RequestUserPassswordResetCommand) {
         const userResult = await this.userService.findOneByEmail(command.email);
-        if (userResult.isErr())
+
+        if (userResult.isNone())
             throw new ConflictException(userResult.unwrap());
+
         const token = this.tokenFactory.generate({ email: command.email });
         const passwordResetRequest = PasswordResetRequestAggregate.create({
             user: userResult.unwrap(),
             passwordResetToken: token,
         });
 
+        const savingRequestResult = await this.passwordResetReqRepo.save(
+            passwordResetRequest
+        );
+
+        if (savingRequestResult.isErr())
+            throw new InternalServerErrorException(
+                'Unable to proceed this action'
+            );
+
         // TODO: Email the otpcode to user
 
-        this.logger.log('Password reset requested for ' + command.email);
+        const props = passwordResetRequest.getProps();
+        return {
+            token: props.passwordResetToken.resetToken,
+            email: props.user.getProps().email,
+        };
     }
 }
