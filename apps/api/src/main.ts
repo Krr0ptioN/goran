772b2db9d2698c4a9a618f643b205bbc7a199414
Krr0ptioN <mardin-pop@protonmail.com>
2024-09-06 03:55:17 +0300

    refact(bootstrap): moving port settings to app config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,24 +4,25 @@ import { CONFIG_APP } from '@goran/config';
 import { AppModule } from './app/app.module';
 import { setupSwagger, registerGlobals } from './bootstrap';
 import { goranBanner } from '@goran/utils';
-import config from './app/app.config';
+import appConfig from './app/app.config';
 
 async function bootstrap() {
     const logger = new Logger('BOOTSTRAP');
 
     logger.log(goranBanner);
 
-    const app = await NestFactory.create(AppModule.register(config()));
+    const config = appConfig();
+    const app = await NestFactory.create(AppModule.register(config));
     const port = process.env[CONFIG_APP.SERVER_PORT] || 3000;
     const { globalPrefix } = await registerGlobals(app);
 
     setupSwagger(app);
 
     logger.log(
-        `  Application is running on: http://localhost:${port}/${globalPrefix}`
+        `  Application is running on: http://localhost:${config.port}/${globalPrefix}`
     );
     logger.log(
-        `  Documentation is running on: http://localhost:${port}/${globalPrefix}/docs`
+        `  Documentation is running on: http://localhost:${config.port}/${globalPrefix}/docs`
     );
 
     await app.listen(port);
