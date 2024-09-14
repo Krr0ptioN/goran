89baa536296d1000f5612b7e939adecd0018bf84
Krr0ptioN <mardin-pop@protonmail.com>
2024-09-14 22:12:27 +0300

    refact(users): moving users to core from domain

--- libs/domain/users/src/domain/events/user-email-updated.domain-event.ts
+++
@@ -1,7 +0,0 @@
-import { DomainEvent, DomainEventProps } from '@goran/common';
-
-export class UserDeletedDomainEvent extends DomainEvent {
-  constructor(props: DomainEventProps<UserDeletedDomainEvent>) {
-    super(props);
-  }
-}
