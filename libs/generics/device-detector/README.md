2c0380ed46d560e6f8e9cbbe070dd1317872d6dd
Krr0ptioN <mardin-pop@protonmail.com>
2024-09-14 17:30:45 +0300

    feat(device-detection-lib): detecting client device informatoin from user-agent

---
+++ libs/generics/device-detector/README.md
@@ -0,0 +1,7 @@
+# device-detector
+
+Thanks to [node-device-detector](https://github.com/sanchezzzhak/node-device-detector)
+
+## Running unit tests
+
+Run `nx test device-detector` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/generics/device-detector/src/lib/device-detector.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import DeviceDetector from 'node-device-detector';
+
+@Injectable()
+export class DeviceDetectorService {
+    private readonly detector = new DeviceDetector({
+        clientIndexes: true,
+        deviceIndexes: true,
+        deviceAliasCode: false,
+        deviceTrusted: false,
+        deviceInfo: false,
+        maxUserAgentSize: 500,
+    });
+    constructor() { }
+
+    /**
+     * detect
+     */
+    public detect(userAgent: string) {
+        return this.detector.detect(userAgent);
+    }
+
+    /**
+     * @param userAgent - The client user agent
+     * @returns Formatted device name with os and browser name
+     * @example Android (5.0) / Nubia Z7 Max (Chrome Mobile)
+     */
+    public getDevice(userAgent: string) {
+        const result = this.detector.detect(userAgent);
+        return `${result.os.name} (${result.os.version}) / ${result.device.model} (${result.client.name})`;
+    }
+}
