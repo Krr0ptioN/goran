246224d13abb9148e0869cc15695eca46737b8b6
Krr0ptioN <mardin-pop@protonmail.com>
2024-09-15 22:22:13 +0300

    refact(security/tokens-cache): removing token revoking logic since it blongs to session

--- libs/core/security/src/lib/tokens/application/tokens.service.ts
+++ libs/core/security/src/lib/tokens/application/tokens.service.ts
@@ -1,24 +1,14 @@
-import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { InvalidTokenError, TokenValueObject } from '../domain';
 import { UsersService } from '@goran/users';
-import { AggregateID } from '@goran/common';
 import { Request } from 'express';
-import { CacheManagerPort } from './ports';
-
-enum TokenState {
-    REVOKED = 'REVOKED',
-}
 
 @Injectable()
 export class TokensService {
     constructor(
         private readonly jwtService: JwtService,
-        private readonly usersService: UsersService,
-        @Inject('CacheManagerPort')
-        private readonly cacheManager: CacheManagerPort,
-        @Inject('REFRESH_IN') private readonly refreshIn: number
-    ) {}
+        private readonly usersService: UsersService
+    ) { }
 
     async getUserFromToken(token: string) {
         const id = this.jwtService.decode(token)['userId'];
@@ -35,18 +25,6 @@ export class TokensService {
         } catch (error) {
             return false;
         }
-    }
-
-    async isTokenRevoked(token: string): Promise<boolean> {
-        const revokedToken = await this.cacheManager.get(token);
-        const isRevoken = revokedToken
-            ? revokedToken === TokenState.REVOKED
-            : false;
-        return isRevoken;
-    }
-
-    async revokeToken(token: string) {
-        await this.cacheManager.set(token, TokenState.REVOKED, this.refreshIn);
     }
 
     extractTokenFromRequest(req: Request) {
