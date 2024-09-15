3d5beb96f4dc7df55383fbf7655651902cfca896
Krr0ptioN <mardin-pop@protonmail.com>
2024-09-15 22:21:09 +0300

    refact(security/tokens-cache): removing caching logic ports

--- libs/core/security/src/lib/tokens/application/index.ts
+++ libs/core/security/src/lib/tokens/application/index.ts
@@ -1,2 +1,1 @@
 export * from './tokens.service';
-export * from './ports'
--- libs/core/security/src/lib/tokens/application/ports/cache-manager.port.ts
+++
@@ -1,4 +0,0 @@
-export abstract class CacheManagerPort {
-  abstract get(key: string): Promise<unknown>;
-  abstract set(key: string, value: unknown, ttl: number): Promise<void>;
-}
