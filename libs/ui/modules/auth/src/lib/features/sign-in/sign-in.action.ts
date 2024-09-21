7354bf27985ed113d340da9fba721cc3606bdf14
Krr0ptioN <mardin-pop@protonmail.com>
2024-09-21 19:49:31 +0300

    feat(ui-module-auth/sign-in): sign in form with validaiton and server action

--- libs/ui/modules/auth/src/lib/features/sign-in/sign-in.action.ts
+++ libs/ui/modules/auth/src/lib/features/sign-in/sign-in.action.ts
@@ -1,6 +1,37 @@
-import { fetchApi } from '@goran/ui-common';
-import { SignInSchema, SignInValues, initialState } from './schema';
+'use server';
 
-export async function signIn(state, userCredentials: SignInValues) {
-    const apiRes = await fetchApi('/auth/signin', { body: userCredentials });
+import { fetchApi } from '@goran/ui-common';
+import { SignInSchema, SignInValues } from './schema';
+import { setAccessToken, setRefreshToken } from '../../actions';
+
+/**
+ * Server action to handle user sign-in.
+ * @param data - The sign-in data.
+ * @returns Result containing user credentials or an error.
+ */
+export async function signIn(data: SignInValues) {
+    const validatedFields = SignInSchema.safeParse(data);
+
+    if (!validatedFields.success) {
+        return {
+            message: 'Invalid field values',
+            errors: validatedFields.error.flatten().fieldErrors,
+        };
+    }
+    const response = await fetchApi('/auth/sign-in', {
+        method: 'POST',
+        body: JSON.stringify(validatedFields.data),
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw { message: errorData.message };
+    }
+
+    const res = await response.json();
+
+    setAccessToken(res.data.accessToken);
+    setRefreshToken(res.data.refreshToken);
+
+    return { message: res.message, code: res.code };
 }
