edf3cf007bacfc2a69fa362f22b2c59bb1d4b873
Krr0ptioN <mardin-pop@protonmail.com>
2024-09-21 19:49:44 +0300

    feat(ui-module-auth/sign-up): sign up form with validaiton and server action

---
+++ libs/ui/modules/auth/src/lib/features/sign-up/form.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { useFormStatus } from 'react-dom';
+import { Form, Button } from '@goran/ui-components';
+import {
+    FullnameField,
+    EmailField,
+    PasswordField,
+    UsernameField,
+} from '../../components';
+import { signUp } from './sign-up.action';
+import { SignUpSchema, signUpInitialValues, SignUpValues } from './schema';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useFormAction } from '@goran/ui-common';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+export const SignUpForm = () => {
+    const router = useRouter();
+    const { pending } = useFormStatus();
+
+    const form = useFormAction<SignUpValues>({
+        resolver: zodResolver(SignUpSchema),
+        defaultValues: signUpInitialValues,
+    });
+
+    const handleSubmit = async (data: SignUpValues) => {
+        const result = await signUp(data);
+        if (result.errors) {
+            console.error('Sign Up Error:', result.errors);
+        } else {
+            router.push('/');
+        }
+    };
+
+    return (
+        <Form {...form}>
+            <form {...form.submitAction(handleSubmit)}>
+                <FullnameField />
+                <UsernameField />
+                <EmailField />
+                <PasswordField />
+                <Button
+                    className="mt-3 w-full"
+                    type="submit"
+                    disabled={pending}
+                >
+                    Sign Up
+                </Button>
+            </form>
+        </Form>
+    );
+};
--- libs/ui/modules/auth/src/lib/features/sign-up/schema.ts
+++ libs/ui/modules/auth/src/lib/features/sign-up/schema.ts
@@ -1,9 +1,7 @@
 import { z } from 'zod';
 
 export const SignUpSchema = z.object({
-    fullname: z
-        .string()
-        .optional(),
+    fullname: z.string().optional(),
     username: z
         .string()
         .min(5, { message: 'Name must be at least 2 characters long.' })
@@ -12,10 +10,38 @@ export const SignUpSchema = z.object({
     password: z
         .string()
         .min(8, { message: 'Be at least 8 characters long' })
-        .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
+        .regex(/[a-z]/, { message: 'Contain at least one lowercase letter.' })
+        .regex(/[A-Z]/, { message: 'Contain at least one uppercase letter.' })
         .regex(/[0-9]/, { message: 'Contain at least one number.' })
         .regex(/[^a-zA-Z0-9]/, {
             message: 'Contain at least one special character.',
         })
         .trim(),
 });
+
+export type SignUpValues = z.infer<typeof SignUpSchema>;
+
+export type SignUpInitialStatus = {
+    errors: {
+        email: string[] | undefined;
+        username: string[] | undefined;
+        fullname: string[] | undefined;
+        password: string[] | undefined;
+    };
+    message: undefined;
+};
+
+export const signUpInitialState = {
+    errors: {
+        email: undefined,
+        username: undefined,
+        fullname: undefined,
+        password: undefined,
+    },
+    message: undefined,
+};
+
+export const signUpInitialValues = {
+    email: '',
+    password: '',
+};
