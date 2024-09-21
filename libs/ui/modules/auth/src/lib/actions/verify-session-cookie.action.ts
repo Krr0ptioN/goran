8e2ea2c6bec52fa70b6e1566e0909db74337a2ef
Krr0ptioN <mardin-pop@protonmail.com>
2024-09-21 19:47:31 +0300

    feat(ui-module-auth/verify-session): verifying and setting sesion server actions

--- libs/ui/modules/auth/src/lib/actions/verify-session-cookie.action.ts
+++ libs/ui/modules/auth/src/lib/actions/verify-session-cookie.action.ts
@@ -1,14 +1,31 @@
+'use server';
+import 'server-only';
+
+import { getUserInfo } from './get-user-info.action';
+import { refreshToken } from './refresh-token.action';
+
+/**
+ * Main function to verify session cookies.
+ * @param accessCookie - The access token cookie.
+ * @param refreshCookie - The refresh token cookie.
+ * @returns User data or error message.
+ */
 export async function verifySessionCookies({
     accessCookie,
-    refetchCookie,
+    refreshCookie,
 }: {
     accessCookie?: string;
-    refetchCookie?: string;
+    refreshCookie?: string;
 }) {
-    // TODO: check session validity
-    // TODO: If acess token is expired get new one with
-    // TODO:
-    return {
-        userId: 'ulid',
-    };
+    try {
+        return await getUserInfo(accessCookie);
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        try {
+            return await refreshToken(refreshCookie);
+        } catch (refreshError) {
+            console.error('Error refreshing token:', refreshError);
+            return { error: 'Token refresh failed' };
+        }
+    }
 }
