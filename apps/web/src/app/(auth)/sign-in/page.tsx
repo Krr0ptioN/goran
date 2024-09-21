8b1569b436977c4b1c064dc4e5ad682b4282a189
Krr0ptioN <mardin-pop@protonmail.com>
2024-09-21 19:41:46 +0300

    feat(ui/auth): sign up and sign in pages

--- apps/web/src/app/(auth)/sign-in/page.tsx
+++ apps/web/src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,21 @@
-'use client';
+'use server';
 
 import { SignInForm } from '@goran/ui-module-auth';
 import Link from 'next/link';
 
-export function Signin() {
+export default async function SignIn() {
     return (
         <>
             <div className="grid gap-2 text-center">
                 <h1 className="text-3xl font-bold">Sign In</h1>
-                <p className="text-balance text-muted-foreground">
+                <p className="text-balance text-lg text-muted-foreground">
                     Enter your email below to sign-in to your account
                 </p>
             </div>
             <div className="grid gap-4">
                 <SignInForm />
             </div>
-            <div className="mt-4 text-center text-sm">
+            <div className="mt-4 text-center text-md">
                 Don&apos;t have an account?{' '}
                 <Link href="/sign-up" className="underline">
                     Sign up
