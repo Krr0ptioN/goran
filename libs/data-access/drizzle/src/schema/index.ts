bbcde2e6246a2e8ebc9bad1f693c1e62ff6d0577
Krr0ptioN <mardin-pop@protonmail.com>
2024-11-03 02:52:37 +0300

    refact(data-access/drizzle): moving each table to their own file

---
+++ libs/data-access/drizzle/src/schema/index.ts
@@ -0,0 +1,14 @@
+import { UsersDataPgTable } from './users';
+import {
+    PasswordResetRequestsDataPgTable,
+    passwordResetStatusEnum,
+} from './password-reset';
+import { SessionsDataPgTable, sessionStatusEnum } from './sessions';
+
+export default {
+    UsersDataPgTable,
+    PasswordResetRequestsDataPgTable,
+    passwordResetStatusEnum,
+    sessionStatusEnum,
+    SessionsDataPgTable,
+};
---
+++ libs/data-access/drizzle/src/schema/users.ts
@@ -0,0 +1,22 @@
+import { pgTable, text, timestamp, uniqueIndex } from 'drizzle-orm/pg-core';
+import { ulid } from 'ulid';
+
+export const UsersDataPgTable = pgTable(
+    'users',
+    {
+        id: text('id')
+            .primaryKey()
+            .$defaultFn(() => ulid()),
+        fullname: text('fullname'),
+        username: text('name').notNull().unique(),
+        password: text('password').notNull(),
+        email: text('email').notNull().unique(),
+        createdAt: timestamp('created_at').defaultNow().notNull(),
+        updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    },
+    (users) => {
+        return {
+            uniqueIdx: uniqueIndex('unique_idx').on(users.email),
+        };
+    }
+);
