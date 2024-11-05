8794327cdd257070ffd6cf08aec87b6e0ea84ab6
Krr0ptioN <mardin-pop@protonmail.com>
2024-11-05 12:24:51 +0300

    feat(catalog/domain): song entity

---
+++ libs/core/catalog/src/lib/domain/entities/song/song.entity.ts
@@ -0,0 +1,18 @@
+import { AggregateID, Entity } from '@goran/common';
+import { CreateSongProps, SongProps } from './song.types';
+import { ulid } from 'ulid';
+
+export class SongEntity extends Entity<SongProps> {
+    protected readonly _id: AggregateID;
+
+    static create(create: CreateSongProps): SongEntity {
+        const id = ulid();
+        const props: SongProps = { ...create };
+        const song = new SongEntity({ id, props });
+        return song;
+    }
+
+    validate(): void {
+        return;
+    }
+}
