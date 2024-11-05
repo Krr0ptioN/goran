f67338f0c06678fe66295b80bdf611e87fdf01c2
Krr0ptioN <mardin-pop@protonmail.com>
2024-11-05 15:34:34 +0300

    fix(catalog/schema): use owner instead of the 'user' as term

--- libs/data-access/drizzle/src/schema/genres/genres.ts
+++ libs/data-access/drizzle/src/schema/genres/genres.ts
@@ -1,11 +1,15 @@
 import { pgTable, text } from 'drizzle-orm/pg-core';
 import { ulid } from 'ulid';
 import { timestamps } from '../columns.helpers';
+import { UsersTable } from '../users';
 
 export const GenresTable = pgTable('genres', {
     ...timestamps,
     id: text('id')
         .primaryKey()
         .$defaultFn(() => ulid()),
+    ownerId: text('owner_id')
+        .notNull()
+        .references(() => UsersTable.id),
     name: text('name').notNull(),
 });
--- libs/data-access/drizzle/src/schema/playlists/playlists.ts
+++ libs/data-access/drizzle/src/schema/playlists/playlists.ts
@@ -3,12 +3,12 @@ import { ulid } from 'ulid';
 import { timestamps } from '../columns.helpers';
 import { UsersTable } from '../users';
 
-export const PlaylistsTable = pgTable('songs', {
+export const PlaylistsTable = pgTable('playlists', {
     ...timestamps,
     id: text('id')
         .primaryKey()
         .$defaultFn(() => ulid()),
-    userId: text('user_id')
+    ownerId: text('owner_id')
         .notNull()
         .references(() => UsersTable.id),
     description: text('description'),
