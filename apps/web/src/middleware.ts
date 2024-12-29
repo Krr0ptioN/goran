31622a8e2195e135a9205296be4296fd42a4bcdf
Mardin <info@mardin.cc>
2024-12-29 20:39:56 +0300

    temp(web/auth): removing auth

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -6,34 +6,35 @@ const protectedRoutes = ['/'];
 const publicRoutes = ['/sign-in', '/sign-up'];
 
 export default async function middleware(req: NextRequest) {
-    const path = req.nextUrl.pathname;
-    const isProtectedRoute = protectedRoutes.includes(path);
-    const isPublicRoute = publicRoutes.includes(path);
-
-    const accessCookie = cookies().get('session-access')?.value;
-    const refreshCookie = cookies().get('session-refresh')?.value;
-
-    if (isProtectedRoute && !(accessCookie || refreshCookie)) {
-        return NextResponse.redirect(new URL('/sign-in', req.nextUrl));
-    }
-
-    const sessionVerifiicationResult = await verifySessionCookies({
-        accessCookie,
-        refreshCookie,
-    });
-
-    if (isProtectedRoute && sessionVerifiicationResult.isErr())
-        return NextResponse.redirect(new URL('/sign-in', req.nextUrl));
-
-    const session = sessionVerifiicationResult.unwrap();
-
-    if (
-        isPublicRoute &&
-        session.userId &&
-        !req.nextUrl.pathname.startsWith('/')
-    ) {
-        return NextResponse.redirect(new URL('/', req.nextUrl));
-    }
-
-    return NextResponse.next();
+    // return NextResponse.redirect(new URL('/', req.nextUrl));
+    // const path = req.nextUrl.pathname;
+    // const isProtectedRoute = protectedRoutes.includes(path);
+    // const isPublicRoute = publicRoutes.includes(path);
+    //
+    // const accessCookie = cookies().get('session-access')?.value;
+    // const refreshCookie = cookies().get('session-refresh')?.value;
+    //
+    // if (isProtectedRoute && !(accessCookie || refreshCookie)) {
+    //     return NextResponse.redirect(new URL('/sign-in', req.nextUrl));
+    // }
+    //
+    // const sessionVerifiicationResult = await verifySessionCookies({
+    //     accessCookie,
+    //     refreshCookie,
+    // });
+    //
+    // if (isProtectedRoute && sessionVerifiicationResult.isErr())
+    //     return NextResponse.redirect(new URL('/sign-in', req.nextUrl));
+    //
+    // const session = sessionVerifiicationResult.unwrap();
+    //
+    // if (
+    //     isPublicRoute &&
+    //     session.userId &&
+    //     !req.nextUrl.pathname.startsWith('/')
+    // ) {
+    //     return NextResponse.redirect(new URL('/', req.nextUrl));
+    // }
+    //
+    // return NextResponse.next();
 }
