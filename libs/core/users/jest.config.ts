fdcc6f06d690b208cc8944b79983aa40963c53b4
Mardin <mardin-pop@protonmail.com>
2025-08-16 15:32:18 +0300

    chore: linting

--- libs/core/users/jest.config.ts
+++ libs/core/users/jest.config.ts
@@ -1,4 +1,4 @@
-/* eslint-disable */
+ 
 export default {
     displayName: 'users',
     preset: '../../../jest.preset.js',
--- libs/generics/ip-locator/jest.config.ts
+++ libs/generics/ip-locator/jest.config.ts
@@ -1,4 +1,4 @@
-/* eslint-disable */
+ 
 export default {
     displayName: 'ip-locator',
     preset: '../../../jest.preset.js',
