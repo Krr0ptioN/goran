5f65f022f649d1ff638f91f82134386c83b95bbd
Mardin Nasouti <info@mardin.cc>
2026-03-06 23:57:25 +0300

    improve(api): stabilize bootstrap config and persistence wiring

--- libs/generics/files/src/lib/infrastructure/errors/file-upload-failed.error.ts
+++ libs/generics/files/src/lib/infrastructure/errors/file-upload-failed.error.ts
@@ -2,7 +2,7 @@ import { ExceptionBase } from '@goran/common';
 
 export class FileUploadFailedInfra extends ExceptionBase {
     static readonly message = 'File upload failed';
-    public readonly scope = 'infrastructure';
+    public override readonly scope = 'infrastructure';
     public readonly code = 'FILES.UPLOAD_FAILED';
 
     constructor(cause?: Error, metadata?: unknown) {
