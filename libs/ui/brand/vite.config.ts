0998dd85d44808c39fc81e403eccb5028a27ab44
Mardin Nasouti <info@mardin.cc>
2026-03-07 22:03:57 +0300

    fix(ci): prevent brand vite config crash in project graph

--- libs/ui/brand/vite.config.ts
+++ libs/ui/brand/vite.config.ts
@@ -5,17 +5,21 @@ import dts from 'vite-plugin-dts';
 import * as path from 'path';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
     root: __dirname,
     cacheDir: '../../../node_modules/.vite/libs/ui/brand',
 
     plugins: [
         react(),
         nxViteTsPaths(),
-        dts({
-            entryRoot: 'src',
-            tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
-        }),
+        ...(command === 'build'
+            ? [
+                  dts({
+                      entryRoot: 'src',
+                      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+                  }),
+              ]
+            : []),
     ],
 
     // Uncomment this if you are using workers.
@@ -45,4 +49,4 @@ export default defineConfig({
             external: ['react', 'react-dom', 'react/jsx-runtime'],
         },
     },
-});
+}));
